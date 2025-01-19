3f11b5d844459fef8e6c91dc33e74d7594abcc71
Sourav Suman <souravweb2024@gmail.com>
2025-01-19 10:34:49 +0530

    datatypes

---
+++ code.js
@@ -0,0 +1,16 @@
+// use "Strict" that treats all javascript code as its newer version .
+// alert not used with node it is used in browser 
+// code readibility 
+// "Write the code that is easily readable" 
+console.log("Sourav");
+console.log(20);
+// some datatypes 
+// number => 2 to the power 54 ;
+// string "" ;
+//BigInt 
+// boolean ;
+// null => for 0th value ;
+//undefined (not defined anything)
+// symbol => some unique 
+// objects (for sets)
+console.log(typeof "sourav");
