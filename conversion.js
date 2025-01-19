ef53b06ea71290ac61920a54871805dbb73f5230
Sourav Suman <souravweb2024@gmail.com>
2025-01-19 16:08:24 +0530

    conversion

---
+++ conversion.js
@@ -0,0 +1,13 @@
+let score = true;
+console.log(score);
+console.log(typeof(score));
+let newScore = Number(score);
+console.log(newScore);
+console.log(typeof(newScore));
+// null =>  0 in number // undefined not converted and other thing converted in Nan
+// true => 1 , false => 0 
+
+let sc = 0 ;
+ let news = Boolean(sc);
+ console.log(news);
+ console.log(typeof(news));
