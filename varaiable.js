a5e3ad4a14022b5f7789aa6f4e03edc953792d5a
Sourav Suman <souravweb2024@gmail.com>
2025-01-19 09:54:02 +0530

    first commit

---
+++ varaiable.js
@@ -0,0 +1,8 @@
+const name = "SOURAV SUMAN" ;
+let age = 20 ;
+var hobby = "CODING" ;
+health = "HAND PAIN" ;
+console.log(name); // const varaible does not change
+console.table([name , age , hobby , health]);
+// let change under the scope not out of the scope 
+// var can be chnaged anywhere that why it not changed in thisfile because it creates confusion 
